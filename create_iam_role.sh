65732e3e2cb162cb93314850ec92282c2c1bc224
sangameshwar1220 <sangameshwar.patil@geminisolutions.com>
2022-04-06 09:41:52 +0530

    role changed

--- create_iam_role.sh
+++ create_iam_role.sh
@@ -3,16 +3,16 @@ TRUST="{   \"Version\": \"2012-10-17\",   \"Statement\": [     {       \"Effect\
 
 echo '{ "Version": "2012-10-17", "Statement": [ { "Effect": "Allow", "Action": "eks:Describe*", "Resource": "*" } ] }' > /tmp/iam-role-policy
 
-aws iam create-role --role-name codebuild-gem-cpbe-build-service-role1 --assume-role-policy-document "$TRUST" --output text --query 'arn:aws:iam::947681381905:role/service-role/cpbe-role'
+aws iam create-role --role-name cpbe-role --assume-role-policy-document "$TRUST" --output text --query 'Role.Arn'
 
-aws iam put-role-policy --role-name codebuild-gem-cpbe-build-service-role1 --policy-name eks-describe --policy-document file:///tmp/iam-role-policy
+aws iam put-role-policy --role-name cpbe-role --policy-name eks-describe --policy-document file:///tmp/iam-role-policy
 
-aws iam attach-role-policy --role-name codebuild-gem-cpbe-build-service-role1 --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
+aws iam attach-role-policy --role-name cpbe-role --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
 
-aws iam attach-role-policy --role-name codebuild-gem-cpbe-build-service-role1 --policy-arn arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess
+aws iam attach-role-policy --role-name cpbe-role --policy-arn arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess
 
-aws iam attach-role-policy --role-name codebuild-gem-cpbe-build-service-role1 --policy-arn arn:aws:iam::aws:policy/AWSCodeCommitFullAccess
+aws iam attach-role-policy --role-name cpbe-role --policy-arn arn:aws:iam::aws:policy/AWSCodeCommitFullAccess
 
-aws iam attach-role-policy --role-name codebuild-gem-cpbe-build-service-role1 --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+aws iam attach-role-policy --role-name cpbe-role --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 
-aws iam attach-role-policy --role-name codebuild-gem-cpbe-build-service-role1 --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
+aws iam attach-role-policy --role-name cpbe-role --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
